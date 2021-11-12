548c5370fa4232b1b3085b3bacda9e971844e95a
Your Name <prajapatirajat94@gmail.com>
2021-11-12 12:11:16 -0500

    updated file

--- src/day1/Object.java
+++ src/day1/Object.java
@@ -3,23 +3,20 @@ package day1;
 public class Object {
 
 	String name;// global variable or class variable
-	int age;
+	public static int age =30;
 	String gender;
 	
 	public static void main(String[] args) {
-	   
-		int a; // local variable 
-		Object obj = new Object();
-		obj.age =30;
-		obj.gender ="male";
-		obj.name = "Allen";
-		Object obj1 = new Object();
-		obj1.age = 20;
-		obj1.gender = "female";
-		obj1.name ="Dora";
-		
-		 Object obj2=new Object();
-       System.out.println(obj1.name);
-	}
+	   Object on = new Object();
+	   on.emp("rajat", 20);
+	   on.emp("raj", 25);
+	   on.gender="male";
+	   System.out.println(on.gender);
  
+}
+	public void emp(String n,int a) {
+		System.out.println(n);
+		System.out.println(a);
+		
+	}
 }
